64c403eea998c3a1340144adf1d342bd9a82de57
Nayeem29 <nayeembracu29@gmail.com>
2022-01-28 00:17:42 +0600

    showing search sites

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-// let buttonClick = document.getElementById("input-btn")
+const btnEl = document.getElementById("btn-el")
 
-function buttonClicked() {
-    console.log("Button Clicked")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+
+let myLeads = []
+
+btnEl.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    renderLeads()
+})
+
+
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + " " + "</li>"
+        console.log(listItems)
+    }
+    ulEl.innerHTML = listItems
 }
