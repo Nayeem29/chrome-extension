2469667f5cb165ed77d3e57350ac08cb762d44d7
Nayeem29 <nayeembracu29@gmail.com>
2022-01-28 01:18:44 +0600

    extension-done

--- main.js
+++ main.js
@@ -7,16 +7,25 @@ let myLeads = []
 
 btnEl.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+        //clear out the input field
+    inputEl.value = ""
     renderLeads()
 })
 
-
+// const recipient = "james"
+// const email = `hey,${recipient}! how it's going?`
+// console.log(email)
 
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + " " + "</li>"
-        console.log(listItems)
+        listItems += `
+        <li>
+          <a target="_blank" href="${myLeads[i]}">
+            ${myLeads[i]}
+          </a>
+        </li>
+        `
     }
     ulEl.innerHTML = listItems
 }
